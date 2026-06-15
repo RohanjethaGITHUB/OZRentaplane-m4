46a0e900839a009dd07faaa6082ea2b4f456a154
Rohan Jetha <rohanjetha2@gmail.com>
2026-06-15 10:20:37 +0530

    icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,12 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
+        />
         <script dangerouslySetInnerHTML={{ __html: screenshotModeBootScript }} />
         <script
           type="application/ld+json"
